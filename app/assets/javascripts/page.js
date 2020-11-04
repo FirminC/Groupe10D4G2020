7353b61de41a247789228c3a9495458d8f559c96
FirminC <firmin.chataigner@gmail.com>
2020-11-04 17:59:03 +0100

    update

--- app/assets/javascripts/page.js
+++ app/assets/javascripts/page.js
@@ -1,4 +1,132 @@
-function filterFunctionR()
-{
-    
+function toggle_departement(param){
+    document.getElementById("Auvergne-Rhône-Alpes").style.display ="none";
+    document.getElementById("Bourgogne-Franche-Comté").style.display ="none";
+    document.getElementById("Bretagne").style.display ="none";
+    document.getElementById("Centre-Val de Loire").style.display ="none";
+    document.getElementById("Grand Est").style.display ="none";
+    document.getElementById("Guadeloupe").style.display ="none";
+    document.getElementById("Guyane").style.display ="none";
+    document.getElementById("Hauts-de-France").style.display ="none";
+    document.getElementById("Île-de-France").style.display ="none";
+    document.getElementById("La Corse").style.display ="none";
+    document.getElementById("Martinique").style.display ="none";
+    document.getElementById("Mayotte").style.display ="none";
+    document.getElementById("Normandie").style.display ="none";
+    document.getElementById("Nouvelle-Aquitaine").style.display ="none";
+    document.getElementById("Occitanie").style.display ="none";
+    document.getElementById("Pays de la Loire").style.display ="none";
+    document.getElementById("Provence-Alpes-Côte d'Azur").style.display ="none";
+    document.getElementById("Réunion").style.display ="none";
+
+    switch (param){
+        case "Auvergne-Rhône-Alpes" :           show1();
+                                                break;
+        case "Bourgogne-Franche-Comté" :        show2();
+                                                break;
+        case "Bretagne" :                       show3();
+                                                break;
+        case "Centre-Val de Loire" :            show4();
+                                                break;
+        case "Grand Est" :                      show5();
+                                                break;
+        case "Guadeloupe" :                     show6();
+                                                break;
+        case "Guyane" :                         show7();
+                                                break;
+        case "Hauts-de-France" :                show8();
+                                                break;
+        case "Île-de-France" :                  show9();
+                                                break;
+        case "La Corse" :                       show10();
+                                                break;
+        case "Martinique" :                     show11();
+                                                break;
+        case "Mayotte" :                        show12();
+                                                break;
+        case "Normandie" :                      show13();
+                                                break;
+        case "Nouvelle-Aquitaine" :             show14();
+                                                break;
+        case "Occitanie" :                      show15();
+                                                break;
+        case "Pays de la Loire" :               show16();
+                                                break;
+        case "Provence-Alpes-Côte d'Azur" :     show17();
+                                                break;
+        case "Réunion" :                        show18();
+                                                break;
+
+    }
+}
+
+function show1(){
+    document.getElementById("Auvergne-Rhône-Alpes").style.display ="inline";
+}
+
+function show2(){
+    document.getElementById("Bourgogne-Franche-Comté").style.display ="inline";
+}
+
+function show3(){
+    document.getElementById("Bretagne").style.display ="inline";
+}
+
+function show4(){
+    document.getElementById("Centre-Val de Loire").style.display ="inline";
+}
+
+function show5(){
+    document.getElementById("Grand Est").style.display ="inline";
+}
+
+function show6(){
+    document.getElementById("Guadeloupe").style.display ="inline";
+}
+
+function show7(){
+    document.getElementById("Guyane").style.display ="inline";
+}
+
+function show8(){
+    document.getElementById("Hauts-de-France").style.display ="inline";
+}
+
+function show9(){
+    document.getElementById("Île-de-France").style.display ="inline";
+}
+
+function show10(){
+    document.getElementById("La Corse").style.display ="inline";
+}
+
+function show11(){
+    document.getElementById("Martinique").style.display ="inline";
+}
+
+function show12(){
+    document.getElementById("Mayotte").style.display ="inline";
+}
+
+function show13(){
+    document.getElementById("Normandie").style.display ="inline";
+}
+
+function show14(){
+    document.getElementById("Nouvelle-Aquitaine").style.display ="inline";
+}
+
+function show15(){
+    document.getElementById("Occitanie").style.display ="inline";
+}
+
+function show16(){
+    document.getElementById("Pays de la Loire").style.display ="inline";
+}
+
+function show17(){
+    document.getElementById("Provence-Alpes-Côte d'Azur").style.display ="inline";
+}
+
+function show18(){
+    document.getElementById("Réunion").style.display ="inline";
 }
