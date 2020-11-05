019af29082fda4a223bc1794493cf102c2566204
FirminC <firmin.chataigner@gmail.com>
2020-11-05 17:24:07 +0100

    Protection injection sql

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -2,13 +2,13 @@ class PageController < ApplicationController
 
     def index
         requete = "TRUE"
-        if !params[:com].blank?
+        if !(params[:com].blank? || params[:com].include?("="))
             requete += " AND insee_com = #{params[:com]}"
         end
-        if !params[:dept].blank?
+        if !(params[:dept].blank? || params[:dept].include?("="))
             requete += " AND insee_dep = #{params[:dept]}"
         end
-        if !params[:reg].blank?
+        if !(params[:reg].blank? || params[:reg].include?("="))
             requete += " AND insee_reg = #{params[:reg]}"
         end
         @scorescommunes = Scorecommune.where(requete).page(params[:page]).per(200)
