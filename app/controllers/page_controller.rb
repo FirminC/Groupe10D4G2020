f4cd58a85c38c94bd3fd40ce1895a6221c6df3de
FirminC <firmin.chataigner@gmail.com>
2020-11-05 16:24:31 +0100

    update pour requete

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -2,15 +2,16 @@ class PageController < ApplicationController
 
     def index
         requete = "TRUE"
-        if params.has_key?(:com)
+        if !params[:com].blank?
             requete += " AND insee_com = #{params[:com]}"
         end
-        if params.has_key?(:dept)
+        if !params[:dept].blank?
             requete += " AND insee_dep = #{params[:dept]}"
         end
-        if params.has_key?(:reg)
+        if !params[:reg].blank?
             requete += " AND insee_reg = #{params[:reg]}"
         end
         @scorescommunes = Scorecommune.where(requete).page(params[:page]).per(200)
+        @comcoms = Scorecommune.select("nom_com").group("nom_com")
     end
 end
