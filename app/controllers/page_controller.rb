a05ea83698b7666d84329e551e366713c9068a9c
FirminC <firmin.chataigner@gmail.com>
2020-11-05 02:14:51 +0100

    Beaucoup d'update. Possible que cassé pour toi

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,6 +1,6 @@
 class PageController < ApplicationController
 
     def index
-        @scorescommunes = Scorecommune.all;
+        @scorescommunes = Scorecommune.page(params[:page]).per(500)
     end
 end
