881d1682db21fca6ec10509c2b9ce7e0554825e1
FirminC <firmin.chataigner@gmail.com>
2020-11-05 21:58:45 +0100

    update

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -12,7 +12,9 @@ class PageController < ApplicationController
             requete += " AND insee_reg = #{params[:reg]}"
         end
         @scorescommunes = Scorecommune.where(requete).page(params[:page]).per(200)
-        @comcoms = Scorecommune.select("nom_comcom").group("nom_comcom")
         @communes = Scorecommune.select("nom_com").group("nom_com")
+        @comcoms = Scorecommune.select("lib_ci").group("lib_ci")
+        @depts = Scorecommune.select("nom_dep").group("nom_dep")
+        @regs = Scorecommune.select("nom_reg").group("nom_reg")
     end
 end
