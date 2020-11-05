d2e64e0642fb1d591c71f484c6de52aded3425e8
FirminC <firmin.chataigner@gmail.com>
2020-11-05 15:33:50 +0100

    update

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,6 +1,16 @@
 class PageController < ApplicationController
 
     def index
-        @scorescommunes = Scorecommune.page(params[:page]).per(500)
+        requete = "TRUE"
+        if params.has_key?(:com)
+            requete += " AND insee_com = #{params[:com]}"
+        end
+        if params.has_key?(:dept)
+            requete += " AND insee_dep = #{params[:dept]}"
+        end
+        if params.has_key?(:reg)
+            requete += " AND insee_reg = #{params[:reg]}"
+        end
+        @scorescommunes = Scorecommune.where(requete).page(params[:page]).per(200)
     end
 end
