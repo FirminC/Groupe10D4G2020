7d0d437ffc37648f45823ff5b82defa144413367
MaxenceGuinard <maxence.guinard49@gmail.com>
2020-11-05 22:29:11 +0100

    sdgqg

--- app/assets/javascripts/page.js
+++ app/assets/javascripts/page.js
@@ -49,6 +49,7 @@ function showAll_departement(){
 
 // --------------------------------------------------------------------------------------- 
 function select_communes(param) {
+
     if (param === "_blank") {
         showAll_communes();
     }else{
@@ -56,7 +57,7 @@ function select_communes(param) {
         var id_communes_select = document.getElementById("communes").selectedIndex;        
 
         var class_communes_select = document.getElementById('communes').getElementsByTagName('option')[id_communes_select].getAttribute("class");
-
+        alert(class_communes_select);
         if (!(class_communes_select.includes(param))) {
 
             document.getElementById('communes').getElementsByTagName('option')[0].selected = 'selected'; 
@@ -87,38 +88,13 @@ function showAll_communes(params) {
 
 // ---------------------------------------------------------------------------------------
 function select_comcoms(param) {
-    if (param === "_blank") {
-        showAll_comcoms();
-    }else{
-        
-        var id_comcoms_select = document.getElementById("comcoms").selectedIndex;        
-
-        var class_comcoms_select = document.getElementById('comcoms').getElementsByTagName('option')[id_comcoms_select].getAttribute("class");
-
-        if (!(class_comcoms_select.includes(param))) {
-
-            document.getElementById('comcoms').getElementsByTagName('option')[0].selected = 'selected'; 
-        }
-        
-        hide_comcoms();
-
-        var comcoms = document.getElementsByClassName(param);
-        for (let index = 0; index < comcoms.length; index++) {
-            comcoms[index].style.display = "inline";
-        }
-    }
+    
 }
 
 function hide_comcoms() {
-    var comcoms = document.getElementsByClassName("comcoms");
-    for (let index = 0; index < comcoms.length; index++) {
-        comcoms[index].style.display = "none";
-    }
+    
 }
 
 function showAll_comcoms() {
-    var comcoms = document.getElementsByClassName("comcoms");
-    for (let index = 0; index < comcoms.length; index++) {
-        comcoms[index].style.display = "inline";
-    }
+  
 }
